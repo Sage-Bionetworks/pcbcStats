54304898831082528fb4f25cf73cf36444122456
kdaily <kdaily@users.noreply.github.com>
2015-11-19 10:16:53 -0800

    don't use loaded packages, explicitly refer to them

--- R/get450KProbeMapping.R
+++ R/get450KProbeMapping.R
@@ -1,26 +1,22 @@
 get450KProbeMapping <- function(probeIDs, platform='HM450', genome='hg19'){
-  require(FDb.InfiniumMethylation.hg19)
-  require(dplyr)
-  require(plyr)
-  
-  hm450 <- getPlatform(platform = platform, genome = genome)
+  hm450 <- FDb.InfiniumMethylation.hg19::getPlatform(platform = platform, genome = genome)
   probes <- hm450[probeIDs]
-    
-  TSS = getNearestTSS(probes)
+
+  TSS = FDb.InfiniumMethylation.hg19::getNearestTSS(probes)
   TSS = rownameToFirstColumn(TSS,'methProbeIDs')
   TSS = dplyr::select(TSS, methProbeIDs, distance, nearestGeneSymbol, nearestTranscript)
   setnames(TSS,c("distance", "nearestGeneSymbol", "nearestTranscript"),
            c("distanceToTSS", "nearestTSS", "nearestTSS.ID"))
-  
-  Tx = getNearestTranscript(probes)
+
+  Tx = FDb.InfiniumMethylation.hg19::getNearestTranscript(probes)
   Tx = rownameToFirstColumn(Tx, 'methProbeIDs')
   Tx = dplyr::select(Tx, methProbeIDs, distance, nearestGeneSymbol, nearestTranscript)
   setnames(Tx,c("distance", "nearestGeneSymbol", "nearestTranscript"),
            c("distanceToTx", "nearestTx", "nearestTx.ID"))
-  
+
   hm450 = rownameToFirstColumn(hm450, 'methProbeIDs')
-  
-  Annotation = join_all(list(hm450,TSS,Tx), by = 'methProbeIDs', match = 'all')
-  
+
+  Annotation = plyr::join_all(list(hm450,TSS,Tx), by = 'methProbeIDs', match = 'all')
+
   return(list(Annotation = Annotation))
 }
--- R/qc.R
+++ R/qc.R
@@ -5,17 +5,18 @@ corr_two_nominal_vars = function(v, dat){
     dd = data.frame(x=x,y=y)
     dd = dd[rowSums(is.na(dd))==0,]
     sm = chisq.test(dd$y, dd$x) # get a p-value
-    r= assocstats(xtabs(~x+y,data=dd))$cramer # get a R^2 value for categorical vars
+    r= vcd::assocstats(xtabs(~x+y,data=dd))$cramer # get a R^2 value for categorical vars
     pval = sm$p.value
     list(r,pval)
 }
 
 most_variable = function(mat, ntop=100){
-    require(statmod)
     # now let's recalculate the most variable genes with the winsorized matrix (wed)
-    means = rowMeans(mat); vars = apply(mat,1,var); cv2 <- vars/means^2
+    means = rowMeans(mat)
+    vars = apply(mat,1,var)
+    cv2 <- vars/means^2
     useForFit <- means >= unname( quantile( means[ which( cv2 > .3 ) ], .95 ) )
-    fit <- glmgam.fit( cbind( a0 = 1, a1tilde = 1/means[useForFit] ),cv2[useForFit] )
+    fit <- statmod::glmgam.fit( cbind( a0 = 1, a1tilde = 1/means[useForFit] ),cv2[useForFit] )
     afit <- fit$coef["a1tilde"]/means+fit$coef["a0"]
     xg <- exp(seq( min(mat), max(mat), length.out=1000 ))
     vfit <- fit$coef["a1tilde"]/xg+fit$coef["a0"]
